c830a593337243922408dbf654771c50d162ca42
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-07 10:35:16 +0530

    Array and Hashing S-2

---
+++ Arrays and Hashing/Code-4.cpp
@@ -0,0 +1,27 @@
+// Valid Anagrams 
+// https://leetcode.com/problems/group-anagrams/
+
+string getKey(string str){
+        vector<int> count(26);
+        for(int i = 0 ; i<str.size() ; i++){
+            count[str[i] - 'a']++ ; 
+        }
+        string key = "" ;
+        for(int i =  0; i<26 ; i++){
+            key += to_string(count[i] + '#');
+        }
+        return key ;
+    }
+  
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<string>> check ;
+        for(int i = 0 ; i<strs.size() ; i++){
+            string key = getKey(strs[i]);
+            check[key].push_back(strs[i]);
+        }
+        vector<vector<string>> ans ;
+        for(auto &pair : check){
+            ans.push_back(pair.second);
+        }
+        return ans ;
+    }
