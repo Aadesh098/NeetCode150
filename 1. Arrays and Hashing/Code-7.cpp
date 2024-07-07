7a4329412b39b92ea4a8561a38a6974900309bbe
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-07 14:29:18 +0530

    Arrays and Hashing S-5

---
+++ 1. Arrays and Hashing/Code-7.cpp
@@ -0,0 +1,17 @@
+// Product of Array Except Self
+// https://leetcode.com/problems/product-of-array-except-self/
+
+vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(),1) ;
+        int prefix = 1 ;
+        for(int i = 0 ; i<nums.size() ; i++){
+            ans[i] = prefix ;
+            prefix *= nums[i];
+        }
+        int postfix = 1 ;
+        for(int i = nums.size()-1 ; i>=0 ; i--){
+            ans[i] *= postfix ;
+            postfix *= nums[i] ;
+        }
+        return ans ;
+    }
