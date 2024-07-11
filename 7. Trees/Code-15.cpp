eecaea5e540636427939190ebc6452feafa21e4e
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-11 09:43:39 +0530

    Trees S-6.1

--- 7. Trees/Code-15.cpp
+++ 7. Trees/Code-15.cpp
@@ -1,33 +1,37 @@
 // Serialize and Deserialize Binary Tree
 // https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
 
-void encode(TreeNode* root, ostringstream& out) {
+void encode(TreeNode* root, string& out) {
         if (root == NULL) {
-            out << "N ";
+            out += "N ";
             return;
         }
-        out << root->val << " ";
+        out += to_string(root->val) + " ";
         encode(root->left, out);
         encode(root->right, out);
     }
 
-    TreeNode* decode(istringstream& in) {
-        string value = "";
-        in >> value;
-        if (value == "N") {
+    TreeNode* decode(const string& data, int& pos) {
+        if (pos >= data.size())
+            return NULL;
+        if (data[pos] == 'N') {
+            pos += 2;
             return NULL;
         }
+        int space_pos = data.find(' ', pos);
+        string value = data.substr(pos, space_pos - pos);
+        pos = space_pos + 1;
         TreeNode* root = new TreeNode(stoi(value));
-        root->left = decode(in);
-        root->right = decode(in);
+        root->left = decode(data, pos);
+        root->right = decode(data, pos);
         return root;
     }
     string serialize(TreeNode* root) {
-        ostringstream out;
+        string out;
         encode(root, out);
-        return out.str();
+        return out;
     }
-    TreeNode* deserialize(string data) {
-        istringstream in(data);
-        return decode(in);
+    TreeNode* deserialize(const string& data) {
+        int pos = 0;
+        return decode(data, pos);
     }
