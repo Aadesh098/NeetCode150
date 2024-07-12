6ee0b8ab39c4e6801fa8f24c80901774de3e5217
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-12 09:44:12 +0530

    Backtracking S-3

---
+++ 9. Backtacking/Code-9.cpp
@@ -0,0 +1,35 @@
+// N-Queens
+// https://leetcode.com/problems/n-queens/
+
+unordered_set<int> cols;
+    unordered_set<int> negDiag;  
+    unordered_set<int> posDiag; 
+    void backtrack(int n, int row, vector<vector<string>>& res, vector<string>& board){
+        if(row==n){
+            res.push_back(board);
+            return ; 
+        }
+        for(int col = 0; col < n; col++){  
+            if( cols.find(col) != cols.end() or 
+                negDiag.find(row - col) != negDiag.end() or 
+                posDiag.find(row + col) != posDiag.end()    
+              )
+                continue;
+            
+            cols.insert(col);
+            negDiag.insert(row - col);
+            posDiag.insert(row + col);
+            board[row][col] = 'Q';
+            backtrack(n, row +1, res, board);
+            cols.erase(col);
+            negDiag.erase(row - col);
+            posDiag.erase(row + col);
+            board[row][col] = '.';
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> board(n, string(n,'.'));
+        backtrack(n, 0, res, board);
+        return res;
+    }
