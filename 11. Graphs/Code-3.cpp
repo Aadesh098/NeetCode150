2f15db711ee797e66989ddbfff25c72af80ce0a0
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-12 15:55:25 +0530

    Graphs S-1

---
+++ 11. Graphs/Code-3.cpp
@@ -0,0 +1,26 @@
+// Clone a Graph
+// https://leetcode.com/problems/clone-graph/
+
+unordered_map<Node*, Node*> m;
+    Node* cloneGraph(Node* node) {
+        if (node == NULL) {
+            return NULL;
+        }
+        Node* copy = new Node(node->val);
+        m[node] = copy;
+        queue<Node*> q;
+        q.push(node);
+        while (!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+            for (int i = 0; i < curr->neighbors.size(); i++) {
+                Node* neighbor = curr->neighbors[i];
+                if (m.find(neighbor) == m.end()) {
+                    m[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);
+                }
+                m[curr]->neighbors.push_back(m[neighbor]);
+            }
+        }
+        return copy;
+    }
