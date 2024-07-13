1943b9fba709b6364470ae3d5921746e3cfdf795
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-13 10:05:46 +0530

    Graphs S-3

---
+++ 11. Graphs/Code-8.cpp
@@ -0,0 +1,35 @@
+// Course Schedule
+// https://leetcode.com/problems/course-schedule/description/
+
+bool dfs(int course, unordered_map<int, vector<int>>& m,
+             unordered_set<int>& visited) {
+        if (visited.find(course) != visited.end()) {
+            return false;
+        }
+        if (m[course].empty()) {
+            return true;
+        }
+        visited.insert(course);
+        for (int i = 0; i < m[course].size(); i++) {
+            int nextCourse = m[course][i];
+            if (!dfs(nextCourse, m, visited)) {
+                return false;
+            }
+        }
+        m[course].clear();
+        visited.erase(course);
+        return true;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> m;
+        for (int i = 0; i < prerequisites.size(); i++) {
+            m[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        unordered_set<int> visited;
+        for (int course = 0; course < numCourses; course++) {
+            if (!dfs(course, m, visited)) {
+                return false;
+            }
+        }
+        return true;
+    }
