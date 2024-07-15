9d54d273f914ae1803e2974dff2bd9bc67172683
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-15 17:48:18 +0530

    1D-DP S-3.1

--- 13_1D_Dynamic_Programming/Code-9.cpp
+++ 13_1D_Dynamic_Programming/Code-9.cpp
@@ -4,10 +4,8 @@
 int maxProduct(vector<int>& nums) {
         int res = nums[0];
         int curMin = 1, curMax = 1;
-        for(int i = 0; i < nums.size(); i++)
-        {
+        for(int i = 0; i < nums.size(); i++){
             int n = nums[i];
-                
             int tmp = curMax * n;
             curMax = max(max(n * curMax, n * curMin), n);
             curMin = min(min(tmp, n * curMin), n);
