3e3e29dc5aa5744f4a87520e628c824c17321c08
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-15 11:52:52 +0530

    1D-DP S-1

---
+++ 13_1D_Dynamic_Programming/Code-2.cpp
@@ -0,0 +1,13 @@
+// Min Cost Climbing Stairs
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+
+int minCostClimbingStairs(vector<int>& cost) {
+        int downOne = 0;
+        int downTwo = 0;
+        for (int i = 2; i <= cost.size(); i++) {
+            int temp = downOne;
+            downOne = min(downOne + cost[i - 1], downTwo + cost[i - 2]);
+            downTwo = temp;
+        }
+        return downOne;
+    }
---
+++ 13_1D_Dynamic_Programming/Code-3.cpp
@@ -0,0 +1,14 @@
+// House Robber
+// https://leetcode.com/problems/house-robber/
+
+int rob(vector<int>& nums) {
+        int prev = 0;
+        int curr = 0;
+        int next = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            next = max(prev + nums[i], curr);
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
