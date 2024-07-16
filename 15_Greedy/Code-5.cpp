7b4a02371101a675eac9e18070e9221446d49daf
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-16 16:29:21 +0530

    Greedy S-2

---
+++ 15_Greedy/Code-5.cpp
@@ -0,0 +1,26 @@
+// Hand of Straights
+// https://leetcode.com/problems/hand-of-straights/
+
+bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if (n % groupSize != 0) {
+            return false;
+        }
+        map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            m[hand[i]]++;
+        }
+        while (!m.empty()) {
+            int curr = m.begin()->first;
+            for (int i = 0; i < groupSize; i++) {
+                if (m[curr + i] == 0) {
+                    return false;
+                }
+                m[curr + i]--;
+                if (m[curr + i] < 1) {
+                    m.erase(curr + i);
+                }
+            }
+        }
+        return true;
+    }
