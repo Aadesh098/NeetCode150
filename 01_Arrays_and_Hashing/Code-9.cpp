a660623cfdac0064a1b2d92814d6d2a22e1dc783
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-28 10:04:01 +0530

    1.3

--- 01_Arrays_and_Hashing/Code-9.cpp
+++ 01_Arrays_and_Hashing/Code-9.cpp
@@ -2,16 +2,16 @@
 // https://leetcode.com/problems/longest-consecutive-sequence/description/
 
 int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
-        int ans = 0;
-        for (auto n : s) {
-            if (s.find(n - 1) == s.end()) {
-                int length = 1;
-                while (s.find(n + length) != s.end()) {
-                    length++;
-                }
-                ans = max(ans, length);
+        unordered_set<int> check(nums.begin() , nums.end());
+        int ans = 0 ;
+        for(auto it : check){
+            if(check.find(it-1) == check.end()){
+                int length = 1 ;
+                while(check.find(it+length) != check.end()){
+                    length++ ;
+                }  
+                ans = max(ans , length);
             }
         }
-        return ans;
+        return ans ;
     }
--- 02_Two_Pointers/Code-1.cpp
+++ 02_Two_Pointers/Code-1.cpp
@@ -1,17 +1,6 @@
 // Valid Palindrome
 // https://leetcode.com/problems/valid-palindrome/
 
-// Algorithm 
-// 1. Create two pointers i=0 & j=n-1 
-
-// 2. Traverse the Array till (i<j)
-// 3. If s[i] != alnum -> i++ 
-// 4. If s[j] != alnum -> j-- 
-// 5. If lower(s[i] != s[j]) -> return False 
-// 6. Else Update Pointers i++ , j-- 
-
-// 7. If no discrepencies found -> return True  
-
 bool isPalindrome(string s) {
         int i = 0 ;
         int j = s.size()-1 ;
