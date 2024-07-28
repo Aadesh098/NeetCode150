27b5b52dabce89692b6ecdceaef72d5a7ec8731f
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-28 22:01:29 +0530

    S-3.2

--- 03_Sliding_Window/Code-3.cpp
+++ 03_Sliding_Window/Code-3.cpp
@@ -1,22 +1,6 @@
 // Longest Repeating Character Replacement
 // https://leetcode.com/problems/longest-repeating-character-replacement/description/
 
-// Algorithm 
-// 1. Create a vector count (stores the frequency of each element)
-// 2. Create 2 pointer i,j to represent the sliding window 
-// 3. Create a variable ans=0 to store the maxSize of the window 
-// 4. Create a variable maxCount=0 to store the maxCount element's frequency 
-
-// 5. Traverse the array till (j<s.size())
-// 6. Increment the frequency of current element 
-// 7. Check if the maxCount needs to be updated 
-// 8. Initiate a loop to validate the window (j-i+1-maxCount > k) 
-// 9. Decrement the starting elements frequencies and update i accordingly 
-// 10. Update Answer if the current window is largest
-// 11. Increment j 
-
-// 12. Return the Final Ans
-
 int characterReplacement(string s, int k) {
         vector<int> count(26);
         int i = 0, j = 0;
--- 03_Sliding_Window/Code-4.cpp
+++ 03_Sliding_Window/Code-4.cpp
@@ -1,21 +1,6 @@
 // Permutation in a String
 // https://leetcode.com/problems/permutation-in-string/description/
 
-// Algorithm 
-// 1. Create variables m,n to store size of the two strings 
-// 2. If s1 size > s2 size -> return false 
-// 3. Create a vector count -> stores the frequency of each alphabet
-
-// 4. Traverse both strings 0->m
-// 5. Increase Frequencies of s1 elements and Decrease Frequencies of s2 elements 
- 
-// 6. Check if all alphabets frequency is 0. If yes -> return true 
-// 7. Traverse the s2 stiring from m->n 
-// 8. Increment the count of s2[i-m] elements and Decrement the count of s2[i] elements
-// 9. Check if all alphabets frequency is 0. If yes -> return true 
-
-// 10. If no valid permutation exists -> return false 
-
 bool isPermutation(vector<int>& count) {
         for (int i = 0; i < 26; i++) {
             if (count[i] != 0) {
