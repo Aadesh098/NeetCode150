06acf882eb74fe78181e48afe83c2c347049ebfb
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-29 09:51:22 +0530

    S-3.3

--- 03_Sliding_Window/Code-4.cpp
+++ 03_Sliding_Window/Code-4.cpp
@@ -1,34 +1,34 @@
 // Permutation in a String
 // https://leetcode.com/problems/permutation-in-string/description/
 
-bool isPermutation(vector<int>& count) {
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                return false;
+bool isPermutation(vector<int> &count){
+        for(int i = 0 ; i<26 ; i++){
+            if(count[i] != 0){
+                return false ;
             }
         }
         return true;
     }
     bool checkInclusion(string s1, string s2) {
-        int m = s1.size();
-        int n = s2.size();
-        if (m > n) {
-            return false;
+        int m = s1.size() ;
+        int n = s2.size() ;
+        if(m>n){
+            return false ;
         }
-        vector<int> count(26, 0);
-        for (int i = 0; i < m; i++) {
-            count[s1[i] - 'a']++;
-            count[s2[i] - 'a']--;
+        vector<int> count(26,0);
+        for(int i = 0 ; i<m ; i++){
+            count[s1[i] - 'a']++ ;
+            count[s2[i] - 'a']-- ;
         }
-        if (isPermutation(count)) {
-            return true;
+        if(isPermutation(count)){
+            return true ;
         }
-        for (int i = m; i < n; i++) {
-            count[s2[i] - 'a']--;
-            count[s2[i - m] - 'a']++;
-            if (isPermutation(count)) {
-                return true;
+        for(int i = m ; i<n ; i++){
+            count[s2[i-m] - 'a']++ ;
+            count[s2[i] - 'a']-- ;
+            if(isPermutation(count)){
+                return true ;
             }
         }
-        return false;
+        return false ;
     }
--- 03_Sliding_Window/Code-5.cpp
+++ 03_Sliding_Window/Code-5.cpp
@@ -1,23 +1,6 @@
 // Minimum Window Substring 
 // https://leetcode.com/problems/minimum-window-substring/description/
 
-// Algorithm 
-// 1. Create an unordrered_map to store char elements -> frequencies 
-// 2. Traverse the string t and increment the frequencies of elements 
-// 3. Create 2 pointers i=0 , j=0 to depict the size of the window 
-// 4. Create 2 variables minStart=0 and minLength=inf to depict the smallest valid window
-// 5. Create a variable counter=t.size() -> chars needed to match t string
-
-// 6. Traverse string s till (s<s.size())
-// 7. Check if elements frequency > 0 -> counter-- 
-// 8. Decrement the frequency as we have the element & increment j
-// 9. Validate the window -> While counter == 0 , check if the size of window is minimum 
-// 10. Update the minStart and minLength pointers accorindgly 
-// 11. Shrink the Size of window -> check[s[i]]++
-// 12. If the element frequency > 0 -> counter++ , increment i 
-
-// 13. If minLenght == INT_MAX return "" , else return the min substring
-
 string minWindow(string s, string t) {
         unordered_map<char, int> check;
         for (int i = 0; i < t.size(); i++) {
