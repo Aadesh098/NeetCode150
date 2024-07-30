3a284b23c146b3eb7bb049bf374f67d3fb34f704
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-30 09:52:12 +0530

    S-4.1

--- 04_Stack/Code-1.cpp
+++ 04_Stack/Code-1.cpp
@@ -1,15 +1,6 @@
 // Valid Parentheses
 // https://leetcode.com/problems/valid-parentheses/description/
 
-// Alrogithm 
-// 1. Create a stack to store string elements 
-// 2. Traverse the string 
-// 3. If stack is empty -> push s[i]
-// 4. Else -> Check if the stack.top element is an opening bracket nd the curr element is a closing bracket -> pop
-// 5. If none of the conditions meet -> push s[i]
-
-// Return Boolean -> ans.empty() 
-
 bool isValid(string s) {
         stack<char> ans;
         for (int i = 0; i < s.length(); i++) {
--- 04_Stack/Code-2.cpp
+++ 04_Stack/Code-2.cpp
@@ -1,20 +1,6 @@
 // Min Stack
 // https://leetcode.com/problems/min-stack/
 
-// Algorithm 
-// 1. Create a vector of pairs to implement the stack
-// 2. If no value exists in the array -> push {val,val}
-// 3. Else push {val , min(val,previous min element)}
-
-// 4. If array is empty -> return 
-// 5. Else pop the last element
-
-// 6. If array is empty -> return -1 
-// 7. Else return arr.back().first 
-
-// 8. If array is empty -> return -1 
-// 9. Else retunr arr.back().second
-
 vector<pair<int, int>> arr;
     MinStack() {}
     void push(int val) {
