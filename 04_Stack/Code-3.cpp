ce6e7bdb1db23cd675e595a69bdce32b9f21f5de
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-30 10:05:15 +0530

    S-4.2

--- 04_Stack/Code-3.cpp
+++ 04_Stack/Code-3.cpp
@@ -1,16 +1,6 @@
 // Evaluate Reverse Polish Notation
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
 
-// Algorithm 
-// 1. Create a stack to store the numeric elements
-// 2. Traverse all the tokens 
-// 3. Extract the current token & check if it is a digit(isdigit(token[0])
-// 4. If yes -> push(stoi(token)) & skip iteration 
-// 5. Else extract the top 2 elements and perform the operation 
-// 6. Store the result back in the stack 
-
-// 7. Finally return the top element in the stack 
-
 int evalRPN(vector<string>& tokens) {
         stack<int> stk;
         for (int i = 0; i < tokens.size(); i++) {
