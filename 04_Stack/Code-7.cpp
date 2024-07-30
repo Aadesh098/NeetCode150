1ff6327abb1aa323254e6d7f31c6cf8029ca2413
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-30 11:14:24 +0530

    S-4.3

--- 04_Stack/Code-7.cpp
+++ 04_Stack/Code-7.cpp
@@ -1,21 +1,6 @@
 // Largest Rectangle in Histogram
 // https://leetcode.com/problems/largest-rectangle-in-histogram/
 
-// Algorithm
-// 1. Create a stack pair<int,int> to store the index and height of the element
-// 2. Create an ans variable to store the maxArea
-// 3. Traverse the complete input array
-// 4. Create a variable start=i , to extend the histogram to left
-// 5. Initiate a loop If stack not empty & previous height is larger
-// 6. Calculate it's area and pop it 
-// 7. Update ans and start=index
-// 8. Push {start,heights[i]} to the stack
-
-// 9. Calculate the area for bars that made till the end 
-// 10. Update ans accordingly 
-
-// 11. Return ans
-
 int largestRectangleArea(vector<int>& heights) {
         stack<pair<int, int>> stk;
         int ans = 0;        
