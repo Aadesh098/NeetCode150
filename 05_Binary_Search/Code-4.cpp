7aa86b54f524a260a23d6075da2622d4bdb67103
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-08-01 13:31:22 +0530

    S-5.2

--- 05_Binary_Search/Code-4.cpp
+++ 05_Binary_Search/Code-4.cpp
@@ -1,18 +1,6 @@
 // Find Minimum in Rotated Sorted Array
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
-// Algorithm
-// 1. Declare 2 pointers start=0 , end=n-1 
-// 2. Check if the input array is not rotated (nums[start] <= nums[end]) , if yes -> return the first element 
-// 3. Initiate a loop till (start <= end)
-// 4. Evaluate the mid element 
-// 5. Check if we are at the pivot element (mid > 0 && nums[mid - 1] > nums[mid]) or nums[mid + 1] < nums[mid)
-// 6. Return the relevant min element 
-// 7. If the current subarray (nums[start]<=nums[mid]) is sorted move the mid to right part
-// 8. Else move the mid to left part 
-
-// 9. Return -1 if invalid input 
-
 int findMin(vector<int>& nums) {
         int start = 0;
         int end = nums.size() - 1;
--- 05_Binary_Search/Code-5.cpp
+++ 05_Binary_Search/Code-5.cpp
@@ -1,16 +1,6 @@
 // Search in Rotated Sorted Array
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 
-// Algorithm 
-// 1. Declare 2 pointers start=0 , end=n-1 
-// 2. Initiate a loop till (start<=end)
-// 3. Evaluate mid & check if the target element exists at mid , if yes -> return mid
-// 4. If the left subarray is sorted (nums[start] <= nums[mid]) 
-// 5. Check if the target element exists in it , if yes -> (end=mid-1) , if no -> (start=mid+1) 
-// 6. Else if the right subarray is sorted 
-// 7. Check if the target element exists in it , if yes -> (start=mid+1) , if no -> (end=mid-1)
-
-// 8. If no element found -> return -1 
 int search(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
