167682bf273be01c18f2851c89c2530594c198fb
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-08-03 09:27:38 +0530

    Check

--- 06_Linked_List/Code-1.cpp
+++ 06_Linked_List/Code-1.cpp
@@ -1,15 +1,6 @@
 // Reverse Linked List 
 // https://leetcode.com/problems/reverse-linked-list/description/
 
-// Algorithm 
-// 1. If list is empty or has a single node -> return head
-// 2. Create 2 variables prev=NULL , curr=head to help with reversal
-// 3. Initiate a loop till (curr)
-// 4. Create a variable temp to store curr->next 
-// 5. Reverse the node 
-
-// 6. Return prev
-
 ListNode *reverseList(ListNode *head){
         if (head == NULL || head->next == NULL){
             return head;
